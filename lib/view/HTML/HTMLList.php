386a59d3002b5c247f82968a20a206da76e594a8
Layton Smith <laytonsmith14@gmail.com>
2012-07-03 13:19:45 -0500

    Fixed a validation issue with lists.

--- lib/view/HTML/HTMLList.php
+++ lib/view/HTML/HTMLList.php
@@ -3,7 +3,14 @@ include_once(dirname(__FILE__)."/HTMLBlock.php");
 abstract class HTMLList extends HTMLBlock{
     public function __construct(array $list){
         foreach($list as $li){
-            if(!($li instanceof HTMLListItem) && !($li instanceof HTMLList)){
+	    if($li instanceof HTMLList){
+		//TODO: If the list already has a list style on it, we don't
+		//need to add our own list-style.
+		$new = new HTMLListItem($li);		
+		$new->addStyle("list-style", "none");
+		$li = $new;
+	    }
+            if(!($li instanceof HTMLListItem)){
                 $li = new HTMLListItem($li);
             }
             $this->addView($li);
